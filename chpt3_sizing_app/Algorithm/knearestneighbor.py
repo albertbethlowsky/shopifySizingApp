42404138de6b9bd549b3a1c3493e90bbc49d5774
frvo <frvo@itu.dk>
2021-04-02 10:50:28 +0200

    knn
    Signed-off-by: frvo <frvo@itu.dk>

---
+++ chpt3_sizing_app/Algorithm/knearestneighbor.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Assign colum names to the dataset
+names = ['Age', 'Height', 'Weight', 'TummyShape', 'HipShape', 'ChestShape', 'Size']
+
+# Read dataset to pandas dataframe
+dataset = pd.read_csv('users.csv', names=names)
+
+
+# #print(len(dataset.index)) #number of items
+# X = dataset.iloc[:, :-1].values #size
+# y = dataset.iloc[:, 6].values #Age, height, weight, tummyshape, hipshape, chestshape
+
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+# from sklearn.preprocessing import StandardScaler
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+
+# X_train = scaler.transform(X_train)
+# X_test = scaler.transform(X_test)
+
+# from sklearn.neighbors import KNeighborsClassifier
+# classifier = KNeighborsClassifier(n_neighbors=3) #len(dataset.index) = number of rows in dataset
+# classifier.fit(X_train, y_train)
+
+# y_pred = classifier.predict(X_test)
+
+# from sklearn.metrics import classification_report, confusion_matrix
+# print(confusion_matrix(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+
+
+
+#new try:
+#https://medium.com/codex/machine-learning-k-nearest-neighbors-algorithm-with-python-df94b374ad41
+
+import pandas as pd # data processing
+from termcolor import colored as cl # elegant printing of text
+import seaborn as sb # visualizations
+import matplotlib.pyplot as plt # editing visualizations
+from matplotlib import style # setting styles for plots
+from sklearn.preprocessing import StandardScaler # normalizing data
+from sklearn.neighbors import KNeighborsClassifier # KNN algorithm
+from sklearn.metrics import accuracy_score # algorithm accuracy
+from sklearn.model_selection import train_test_split # splitting the data
+
+style.use('seaborn-whitegrid')
+plt.rcParams['figure.figsize'] = (16, 7)
+
+# #Scatter plot:
+# sb.scatterplot('Height', 'Weight', data = dataset, hue = 'Size', palette = 'Set2', edgecolor = 'b', s = 150, 
+#                alpha = 0.7)
+# plt.title('Height / Weight')
+# plt.xlabel('Height')
+# plt.ylabel('Weight')
+# plt.legend(loc = 'upper left', fontsize = 12)
+# plt.savefig('heightweight.png')
+
+# # 4. Scatter Matrix:
+
+# sb.pairplot(data = dataset, hue = 'Size', palette = ['Red', 'Blue', 'limegreen'])
+# plt.savefig('pairplot.png')
+
+#Training the data
+X_var = dataset[['Age', 'Height', 'Weight', 'TummyShape', 'HipShape', 'ChestShape']].values
+#print(X_var)
+y_var = dataset['Size'].values
+
+# print(cl('X variable :', attrs = ['bold']), X_var[:5])
+# print(cl('Y variable :', attrs = ['bold']), y_var[:5])
+
+#Normalize:
+X_var = StandardScaler().fit(X_var).transform(X_var.astype(float))
+#print(cl(X_var[:5], attrs = ['bold'])) #see dependent and independent variables?
+
+#train:
+X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.3, random_state = 0)
+
+# print(cl('Train set shape :', attrs = ['bold']), X_train.shape, y_train.shape)
+# print(cl('Test set shape :', attrs = ['bold']), X_test.shape, y_test.shape)
+
+#knn algo:
+k = 3
+
+neigh = KNeighborsClassifier(n_neighbors = k)
+neigh.fit(X_train, y_train)
+
+#print(cl(neigh, attrs = ['bold']))
+
+#prediction
+yhat = neigh.predict(X_test)
+
+
+# Assign colum names to the dataset
+names = ['Age', 'Height', 'Weight', 'TummyShape', 'HipShape', 'ChestShape', 'Size']
+
+# Read dataset to pandas dataframe
+dataset = pd.read_csv('testuser.csv', names=names)
+
+testUser = dataset[['Age', 'Height', 'Weight', 'TummyShape', 'HipShape', 'ChestShape']].values
+#print(arrOfArr)
+# newPrediction = { 'Age': [25],
+#                 'Height': [170],
+#                 'Weight': [88],
+#                 'TummyShape': [2],
+#                 'HipShape': [1],
+#                 'ChestShape': [1]
+#                 }
+print(testUser)
+#df = pd.DataFrame(arrOfArr,columns=['Age', 'Weight','Height','TummyShape', 'HipShape', 'ChestShape']) 
+Normalize = StandardScaler().fit(testUser).transform(testUser.astype(float))
+print(Normalize)
+print(X_var)
+print(X_test)
+
+
+
+#print(cl('Prediction Accuracy Score (%) :', attrs = ['bold']), round(accuracy_score(newPrediction, yhat)*100, 2))
